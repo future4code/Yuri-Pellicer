142a388ffdbfcc5b6f82cfd42afacca09b39d6b2
unknown <lukianchukipellicer@gmail.com>
2021-07-16 19:35:42 -0300

    index.js

--- quinzenal1/variaveis/index.js
+++ quinzenal1/variaveis/index.js
@@ -1,15 +1,78 @@
-/*ex1-o primeiro console.log iraa imprimir o valor atribuido a b
-que no exemplo é igual a b que foi 10*/
-/*ex2-segundo console.log vai imprimir o segundo valor atribuido
-a b, que no exemplo foi 5
+// Exercicios de interpretação de codigo//
+/**ex1-o primeiro console.log iraa imprimir o valor atribuido a b
+que no exemplo é igual a b que foi 10
+segundo console.log vai imprimir o segundo valor atribuido
+a b, que no exemplo foi 5 e o valor de A permaneçe
 isso pq não foi estabelecida como conts, logo ela pode ser alterada*/
-/*ex-3let horasTrabalhadas = prompt("quantas horas voce trabalha por dia")
-let salarioDiario = prompt ("quanto voce recebe por dia")
-alert("valor/horas trabalhadas")
-*/
 
-/* let nome
+/* ex2-neste exercicio foi colocado o valor de A igual a 10 
+e o valor de b igual a 20,valor de c não foi colocado.
+porém os valores podem ser alterados por serem colocados com let
+oq permite que o valor de c=a, b=c, a=b
+depois e imprimido com console.log os valores de a, b, c.*/
+
+/*let horasDiarias = prompt("quantas horas você trabalha por dia")
+let recebimentoDiario= ("quanto você recebe por dia")
+alert('voce recebe $ ('horasDiarias/RecebimentoDiario)por hora')*/
+
+
+//exercicio de escrita do codigo
+
+// exercicio A
+let nome
+
+//exercicio b
 let idade
+
+//exercicio c
 console.log(typeof nome)
-console.log(typeof idade)*/
+console.log(typeof idade)
+
+//exercicio d
+//Porque o valor não foi atribuido
+
+//exercicio e
+let nome= prompt("qual o seu nome?")
+let idade= prompt("qual a sua idade?")
+console.log(typeof nome)
+console.log(typeof idade)
+
+//exercicio f
+let nome= prompt("qual o seu nome?")
+let idade= prompt("qual a sua idade?")
+console.log(typeof nome)
+console.log(typeof idade)
+
+let nome=pompt("qual o seu nome?")//string
+let idade=prompt("qual a sua idade?")//number
+
+//exercicio g
+
+console.log("meu nome é,nome,minha idade é,idade,")
+
+//exercicio 2
+
+//exercicio A e B juntos
+alert ('respostas a seguir serão de Sim ou Não')
+let pergunta1= prompt("voce dormiu bem?")
+let pergunta2 = prompt("voce ja comeu chocolate?")
+let pergunta3= prompt("voce bebeu agua hoje?")
+
+let resposta1= resposta1
+let resposta2 = resposta2
+let resposta3= resposta3
+
+console.log(" resposta1, resposta, resposta3")
+
+//exercicio 3
+let a= 10
+let b= 25
+let c
+c=a
+a=b
+b=c
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10
+
+
 
