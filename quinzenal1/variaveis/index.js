5ca3c3a811c0a9922c14d89672dafc5a4a7fd960
unknown <lukianchukipellicer@gmail.com>
2021-07-15 21:54:09 -0300

    index.html

---
+++ quinzenal1/variaveis/index.js
@@ -0,0 +1,15 @@
+/*ex1-o primeiro console.log iraa imprimir o valor atribuido a b
+que no exemplo é igual a b que foi 10*/
+/*ex2-segundo console.log vai imprimir o segundo valor atribuido
+a b, que no exemplo foi 5
+isso pq não foi estabelecida como conts, logo ela pode ser alterada*/
+/*ex-3let horasTrabalhadas = prompt("quantas horas voce trabalha por dia")
+let salarioDiario = prompt ("quanto voce recebe por dia")
+alert("valor/horas trabalhadas")
+*/
+
+/* let nome
+let idade
+console.log(typeof nome)
+console.log(typeof idade)*/
+
