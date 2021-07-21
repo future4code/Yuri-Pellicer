325e2f2e8e95ae9a7202b736547125ebff355650
unknown <lukianchukipellicer@gmail.com>
2021-07-20 21:45:18 -0300

    quinzena1-aula4

---
+++ quinzena1/operadores/template_javascript (1)/template_javascript/index.js
@@ -0,0 +1,49 @@
+// exercicio 1 interpretação de codigo 
+
+/*const bool1 = true 
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado)// false
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) //false
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)//true
+
+console.log("d. ", typeof resultado)// boolean*/
+
+// exercicio 2 interpretação de codigo
+
+/*let primeiroNumero = prompt("Digite o primeiro numero!")
+let segundoNumero = prompt("Digite o segundo numero!")
+
+resultado = primeiroNumero + segundoNumero
+console.log("soma.", resultado)*/
+
+//exercicio 1 escrita do codigo 
+
+/*const idade= prompt("qual sua idade?")
+const idadeAmigo =prompt("qual a idade do seu melhor amigo?")
+
+resultado= idade >= idadeAmigo
+console.log("sua idade é maior que a do seu melhor amigo",resultado)*/
+
+// exerecicio 2
+
+//let numeroPar =number(prompt("insira um numero par!")
+////console.log("o resto é" numeroPar % 2)
+
+// exercicio 3 
+
+
+//exercicio 4
+//const primeiroNumero = prompt("digite um numero!")
+//const segundoNumero = prompt("digite outro numero!")
+
+//console.log(" o primeiro numero é maior que o segundo?",primeiroNumero > segundoNumero)
+//console.log(" o primeiro é igual ao segundo?",primeiroNumero === segundoNumero)
+//console.log("o primeiro numero é divisível pelo segundo?",primeiroNumero % segundoNumero === 0)
+console.log("o segundo numero é divisível pelo primeiro?",segundoNumero % primeiroNumero === 0)
